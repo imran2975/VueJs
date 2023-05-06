c3cd060d2bf632d09a5b85cca9203ebad4e55420
Imran Abubakar <imranabubakar943@gmail.com>
2023-05-06 11:52:38 +0100

    add bootstrap responsive

--- VueX 4 Firebase Auth/src/components/Navbar.vue
+++ VueX 4 Firebase Auth/src/components/Navbar.vue
@@ -1,27 +1,65 @@
 <template>
-  <nav>
-    <h1>Vuex Auth</h1>
-    <!-- for all users -->
-    <template v-if="authIsReady">
-      <div>
-        <router-link to="/">Home</router-link>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light pt-3 pb-3">
+      <div class="container-fluid">
+        <h1 class="navbar-brand" href="#"><a>Vuex Auth</a></h1>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse"
+          id="navbarSupportedContent"
+          ref="navbarMenu"
+        >
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="nav-item">
+              <router-link to="/" class="nav-link active" @click="collapseMenu"
+                >Home</router-link
+              >
+            </div>
+            <div class="nav-item">
+              <router-link to="/" class="nav-link" @click="collapseMenu"
+                >News</router-link
+              >
+            </div>
+            <div class="nav-item">
+              <router-link to="/" class="nav-link" @click="collapseMenu"
+                >Features</router-link
+              >
+            </div>
+            <div class="nav-item">
+              <router-link to="/" class="nav-link" @click="collapseMenu"
+                >Products</router-link
+              >
+            </div>
+
+            <!-- for logged out users -->
+            <div v-if="!user" class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+              <router-link to="/signup" class="nav-link">Signup</router-link>
+            </div>
+          </ul>
+          <!-- for logged in users -->
+          <div v-if="user" class="nav-item">
+            <span>Logged in as {{ user.email }}</span>
+            <button class="btn btn-danger" @click="handleClick">Logout</button>
+          </div>
+        </div>
       </div>
-      <!-- for logged in users -->
-      <div v-if="user">
-        <span>Logged in as {{ user.email }}</span>
-        <button @click="handleClick">Logout</button>
-      </div>
-      <!-- for logged out users -->
-      <div v-if="!user">
-        <router-link to="/login">Login</router-link>
-        <router-link to="/signup">Signup</router-link>
-      </div>
-    </template>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
 
 export default {
@@ -32,8 +70,16 @@ export default {
       store.dispatch("logOut");
     };
 
+    const navbarMenu = ref(null);
+
+    const collapseMenu = () => {
+      navbarMenu.value.classList.remove("show");
+    };
+
     return {
       handleClick,
+      collapseMenu,
+      navbarMenu,
       user: computed(() => store.state.user),
       authIsReady: computed(() => store.state.authIsReady),
     };
