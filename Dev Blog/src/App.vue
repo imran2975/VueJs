621c90eda80e8f549efb5c9284967a0bc314349c
Imran Abubakar <imranabubakar943@gmail.com>
2023-08-25 23:41:51 +0100

    added contact form

--- Dev Blog/src/App.vue
+++ Dev Blog/src/App.vue
@@ -53,7 +53,7 @@ export default {
 }
 
 #content {
-  margin-top: 6rem;
+  margin-top: 5rem;
 }
 
 #navbar,
--- Dev Blog/src/views/ContactUs.vue
+++ Dev Blog/src/views/ContactUs.vue
@@ -1,6 +1,45 @@
 <template>
-  <div>
-    <form action=""></form>
+  <div class="hi">
+    <form action="">
+      <h2>Love to hear from you, Get in touch👋</h2>
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name"
+          required
+        />
+        <label for="floatingInput">Full name</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          required
+        />
+        <label for="floatingInput">Email address</label>
+      </div>
+      <select class="form-select" aria-label="Default select example" required>
+        <option selected>Category</option>
+        <option value="complaint">Complaint</option>
+        <option value="Advice">Advice</option>
+        <option value="Report">Report</option>
+      </select>
+      <div class="form-floating">
+        <textarea
+          class="form-control"
+          placeholder="Leave a comment here"
+          id="floatingTextarea"
+          style="height: 15rem"
+          required
+        ></textarea>
+        <label for="floatingTextarea">Message...</label>
+      </div>
+      <button class="contact-btn btn btn-warning my-2">Submit</button>
+    </form>
   </div>
 </template>
 
@@ -8,4 +47,26 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+}
+button {
+  font-weight: bold;
+}
+
+@media (min-width: 576px) {
+  .hi {
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    background: #ebebeb;
+  }
+  form {
+    width: 50%;
+    border: 1px solid #cac9c9;
+  }
+}
+</style>
